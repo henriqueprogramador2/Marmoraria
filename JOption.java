a0f2b7250173a0885126f7a10c2c7825938dd0ad
Henrique dev <72462260+henriqueprogramador2@users.noreply.github.com>
2021-10-28 18:10:16 -0300

    visão geral (TELAS)

---
+++ JOption.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visaogeral;
+
+import javax.swing.JRootPane;
+
+/**
+ *
+ * @author Programador Henrique
+ */
+class JOption {
+
+    static void showMessageDialog(JRootPane rootPane, String acesso_negado) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
